c8eb8d0a4c06d156a1e052015283dbef69ee1e50
Your Name <85418443+alrammahi-mariia@users.noreply.github.com>
2024-09-11 15:11:54 +0300

    display posts list and add add posts form

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,29 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
-import type { Action } from '@reduxjs/toolkit'
 
-interface CounterState {
-  value: number
-}
+// Removed the `counterReducer` function, `CounterState` type, and `Action` import
 
-// An example slice reducer function that shows how a Redux reducer works inside.
-// We'll replace this soon with real app logic.
-function counterReducer(state: CounterState = { value: 0 }, action: Action) {
-  switch (action.type) {
-    // Handle actions here
-    default: {
-      return state
-    }
-  }
-}
+import postsReducer from '@/features/posts/postsSlice'
 
 export const store = configureStore({
-  // Pass in the root reducer setup as the `reducer` argument
   reducer: {
-    // Declare that `state.counter` will be updated by the `counterReducer` function
-    counter: counterReducer,
+    posts: postsReducer,
   },
 })
-
 // Infer the type of `store`
 export type AppStore = typeof store
 // Infer the `AppDispatch` type from the store itself
